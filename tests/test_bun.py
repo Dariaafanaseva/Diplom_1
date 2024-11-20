537cf4de9fcaa0061bd76c1e40e7e1ac67c7f488
Daria Afanaseva <dariastukalo@yandex.ru>
2024-11-21 00:39:11 +0500

    Задание 1 готово

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,42 +1,41 @@
+import allure
 import pytest
 from practicum.bun import Bun
 from data import BUN_NAME, BUN_PRICE, BUN_PRICE_ZERO, BUN_PRICE_NEGATIVE
 
 class TestBun:
-    def test_bun_initialization(self):
-        bun = Bun(BUN_NAME, BUN_PRICE)
-        assert bun.get_name() == BUN_NAME
-        assert bun.get_price() == BUN_PRICE
+    @pytest.mark.parametrize("name, price, expected", [
+        (BUN_NAME, BUN_PRICE, BUN_PRICE),
+        (BUN_NAME, BUN_PRICE_NEGATIVE, BUN_PRICE_NEGATIVE),
+        (BUN_NAME, BUN_PRICE_ZERO, 0.0)])
 
+    @allure.title("Проверка разных цен булки")
+    def test_bun_price(self, name, price, expected):
+        bun = Bun(name, price)
+        assert bun.get_price() == expected
+
+    @allure.title("Проверка имени булки")
     def test_bun_name(self):
         bun = Bun(BUN_NAME, BUN_PRICE)
         assert bun.get_name() == BUN_NAME
 
-    def test_bun_price(self):
-        bun = Bun(BUN_NAME, BUN_PRICE)
-        assert bun.get_price() == BUN_PRICE
-
-    def test_bun_negative_price(self):
-        bun = Bun(BUN_NAME, BUN_PRICE_NEGATIVE)
-        assert bun.get_price() == BUN_PRICE_NEGATIVE
-
-    def test_bun_zero_price(self):
-        bun = Bun(BUN_NAME, BUN_PRICE_ZERO)
-        assert bun.get_price() == 0.0
-
-    def test_bun_price_type(self):
+    @allure.title("Проверка типа данных цены булки")
+    def test_bun_type_price(self):
         bun = Bun(BUN_NAME, BUN_PRICE)
         assert isinstance(bun.get_price(), float)
 
+    @allure.title("Проверка типа данных названия булки")
     def test_bun_name_type(self):
         bun = Bun(BUN_NAME, BUN_PRICE)
         assert isinstance(bun.get_name(), str)
 
+    @allure.title("Проверка возможности сменить имя булки")
     def test_bun_set_name(self):
         bun = Bun(BUN_NAME, BUN_PRICE)
-        bun.name = "Glutten_free"
-        assert bun.get_name() == "Glutten_free"
+        bun.name = "Glutten"
+        assert bun.get_name() == "Glutten"
 
+    @allure.title("Проверка возможности сменить цену булки")
     def test_bun_set_price(self):
         bun = Bun(BUN_NAME, BUN_PRICE)
         bun.price = 109.9
--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,19 +1,23 @@
 from practicum.burger import Burger
 from data import BUN_NAME, BUN_PRICE, INGREDIENT_NAME, INGREDIENT_PRICE, INGREDIENT2_PRICE, INGREDIENT2_NAME
 from practicum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+import allure
 
 class TestBurger:
+    @allure.title("Проверка инициализации бургера")
     def test_create_burger(self):
         burger = Burger()
         assert burger.bun is None
         assert burger.ingredients == []
 
+    @allure.title("Проверка выбора булочки для бургера")
     def test_set_buns(self, mock_bun):
         burger = Burger()
         burger.set_buns(mock_bun)
         assert burger.bun.get_name() == BUN_NAME
         assert burger.bun.get_price() == BUN_PRICE
 
+    @allure.title("Проверка возможности добавить ингредиент в бургер")
     def test_add_ingredient(self, mock_ingredient1):
         burger = Burger()
         burger.add_ingredient(mock_ingredient1)
@@ -22,12 +26,14 @@ class TestBurger:
         assert burger.ingredients[0].price == INGREDIENT_PRICE
         assert burger.ingredients[0].type == INGREDIENT_TYPE_SAUCE
 
+    @allure.title("Проверка возможности удалить ингредиент из бургера")
     def test_remove_ingredient(self, mock_ingredient1):
         burger = Burger()
         burger.add_ingredient(mock_ingredient1)
         burger.remove_ingredient(0)
         assert len(burger.ingredients) == 0
 
+    @allure.title("Проверка возможности переместить ингредиенты в бургере")
     def test_move_ingredient(self, mock_ingredient1, mock_ingredient2):
         burger = Burger()
         burger.add_ingredient(mock_ingredient1)
@@ -37,29 +43,33 @@ class TestBurger:
         assert burger.ingredients[0].price == INGREDIENT2_PRICE
         assert burger.ingredients[0].type == INGREDIENT_TYPE_FILLING
 
+    @allure.title("Проверка возможности узнать цену бургера")
     def test_get_price_burger(self, mock_bun, mock_ingredient1):
         burger = Burger()
         burger.set_buns(mock_bun)
         burger.add_ingredient(mock_ingredient1)
         assert burger.get_price() == 398.8
 
+    @allure.title("Проверка типа данных цены бургера")
     def test_get_price_burger_type(self, mock_bun, mock_ingredient1):
         burger = Burger()
         burger.set_buns(mock_bun)
         burger.add_ingredient(mock_ingredient1)
         assert isinstance(burger.get_price(), float)
 
+    @allure.title("Проверка распечатки чека")
     def test_get_receipt(self, mock_bun, mock_ingredient1, mock_ingredient2):
         burger = Burger()
         burger.set_buns(mock_bun)
         burger.add_ingredient(mock_ingredient1)
         burger.add_ingredient(mock_ingredient2)
-        total_price = mock_bun.get_price()*2 + mock_ingredient1.get_price() + mock_ingredient2.get_price()
+        burger_price = mock_bun.get_price()*2 + mock_ingredient1.get_price() + mock_ingredient2.get_price()
         expected_receipt = (
             f'(==== {mock_bun.get_name()} ====)\n'  
             f'= {mock_ingredient1.get_type().lower()} {mock_ingredient1.get_name()} =\n'  
             f'= {mock_ingredient2.get_type().lower()} {mock_ingredient2.get_name()} =\n'  
             f'(==== {mock_bun.get_name()} ====)\n'
-            f'\nPrice: {total_price:.1f}')
+            f'\nPrice: {burger_price:.1f}')
+
         assert burger.get_receipt() == expected_receipt
 
